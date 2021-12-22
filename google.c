19963c4ae0fe3a273c66a33d1ce5f8ccb74a6aef
wwwuyan <1836523915@qq.com>
2021-12-22 19:54:53 +0800

    google add commit

--- google.c
+++ google.c
@@ -3,3 +3,40 @@
 //f(1) = 1。
 //求另一个f(n)=n的n值
 // 2021-11-04
+
+#include <stdio.h>
+int main()
+{
+    int a[10];
+    int i;
+    int h=0;
+    int m=1;
+    int x;
+    for(x=1; x<1000000; x++){
+        /*h=0;                       //超时
+        for(int n=1; n<=x; n++){
+            m=n;
+            for(i=0; m>0; i++){
+                a[i]=m%10;
+                m=m/10;
+
+                if(a[i]==1) h++;
+            }
+
+        }*/
+
+        m=x;
+        for(i=0; m>0; i++){
+            a[i]=m%10;
+            m=m/10;
+
+            if(a[i]==1) h++;
+        }
+
+        if(h==x)
+        printf("%d ", h);
+
+    }
+    return 0;
+
+}
